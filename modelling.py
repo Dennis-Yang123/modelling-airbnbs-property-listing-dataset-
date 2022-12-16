f7e8b82667e121770431b94d89de80b1b68768ee
Dennis Yang <dennisyang3.14@gmail.com>
2022-12-16 20:35:58 +0000

    Milestone 4 Task 2

--- modelling.py
+++ modelling.py
@@ -3,6 +3,8 @@ from tabular_data import load_airbnb
 from sklearn.linear_model import SGDRegressor
 from sklearn.model_selection import train_test_split
 import numpy as np
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from math import sqrt
 
 training_data = load_airbnb("Price_Night")
 # print(training_data)
@@ -13,7 +15,15 @@ linear_model = SGDRegressor()
 linear_model.fit(x_train, y_train)
 
 y_test_prediction = linear_model.predict(x_test)
-
-
 print(y_test_prediction)
+
+y_mae = mean_absolute_error(y_test, y_test_prediction)
+y_mse = mean_squared_error(y_test, y_test_prediction)
+y_rmse = sqrt(y_mse)
+y_r2 = r2_score(y_test, y_test_prediction)
+
+print(f"MAE: {y_mae}")
+print(f"MSE: {y_mse}")
+print(f"RMSE: {y_rmse}")
+print(f"R-Squared: {y_r2}")
 # %%
