4726fd92fd5d097731db733874555df674335dd7
Dennis Yang <dennisyang3.14@gmail.com>
2022-12-20 21:04:10 +0000

    Milestone 4 Task 4

--- modelling.py
+++ modelling.py
@@ -5,7 +5,8 @@ from sklearn.model_selection import train_test_split
 import numpy as np
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from math import sqrt
-import itertools
+from sklearn.model_selection import GridSearchCV
+
 
 training_data = load_airbnb("Price_Night")
 # print(training_data)
@@ -16,7 +17,7 @@ linear_model = SGDRegressor()
 linear_model.fit(x_train, y_train)
 
 y_test_prediction = linear_model.predict(x_test)
-print(y_test_prediction)
+# print(y_test_prediction)
 
 y_mae = mean_absolute_error(y_test, y_test_prediction)
 y_mse = mean_squared_error(y_test, y_test_prediction)
@@ -44,10 +45,21 @@ def custome_tune_regression_model_hyperparameters(model, features, label, dict_h
             dict_metric = {"validation_RMSE": score}
 
     return best_params, dict_metric
-dict_hyp = [    {'loss': 'huber', 'penalty': 'l2', 'alpha': 0.1},    {'loss': 'epsilon_insensitive', 'penalty': 'l2', 'alpha': 0.01},    {'loss': 'epsilon_insensitive', 'penalty': 'l2', 'alpha': 0.001},    {'loss': 'huber', 'penalty': 'l2', 'alpha': 0.1},    {'loss': 'huber', 'penalty': 'l2', 'alpha': 0.01},    {'loss': 'huber', 'penalty': 'l2', 'alpha': 0.001},]
+dict_hyp = {
+    'loss': ['huber'],
+    'penalty': ['l2', 'l1', 'elasticnet'],
+    'alpha': [0.1, 0.01, 0.001],
+}
 
+# best_parameters, best_score= custome_tune_regression_model_hyperparameters(linear_model, training_data[0], training_data[1], dict_hyp)
+# print(best_parameters)
+# print(best_score)
 
-best_parameters, best_score= custome_tune_regression_model_hyperparameters(linear_model, training_data[0], training_data[1], dict_hyp)
-print(best_parameters)
-print(best_score)
+def tune_regression_model_hyperparameters():
+    grid_search = GridSearchCV(linear_model, dict_hyp, cv = 5)
+    grid_search.fit(training_data[0], training_data[1])
+    print(grid_search.best_params_)
+
+tune_regression_model_hyperparameters()
+
 # %%
