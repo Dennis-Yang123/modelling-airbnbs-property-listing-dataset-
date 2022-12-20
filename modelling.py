267a18fac836c497845fba8afc2ca10054918058
Dennis Yang <dennisyang3.14@gmail.com>
2022-12-20 20:37:56 +0000

    Milestone 4 Task 3

--- modelling.py
+++ modelling.py
@@ -5,6 +5,7 @@ from sklearn.model_selection import train_test_split
 import numpy as np
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from math import sqrt
+import itertools
 
 training_data = load_airbnb("Price_Night")
 # print(training_data)
@@ -22,8 +23,31 @@ y_mse = mean_squared_error(y_test, y_test_prediction)
 y_rmse = sqrt(y_mse)
 y_r2 = r2_score(y_test, y_test_prediction)
 
-print(f"MAE: {y_mae}")
-print(f"MSE: {y_mse}")
-print(f"RMSE: {y_rmse}")
-print(f"R-Squared: {y_r2}")
+# print(f"MAE: {y_mae}")
+# print(f"MSE: {y_mse}")
+# print(f"RMSE: {y_rmse}")
+# print(f"R-Squared: {y_r2}")
+
+def custome_tune_regression_model_hyperparameters(model, features, label, dict_hyp):
+    best_params = None
+    best_score = float("inf")
+
+    for params in dict_hyp:
+        model = SGDRegressor(**params)
+        model.fit(x_train, y_train)
+        y_pred = model.predict(x_test)
+        score = sqrt(mean_squared_error(y_test, y_pred))
+
+        if score < best_score:
+            best_params = params
+            best_score = score
+            dict_metric = {"validation_RMSE": score}
+
+    return best_params, dict_metric
+dict_hyp = [    {'loss': 'huber', 'penalty': 'l2', 'alpha': 0.1},    {'loss': 'epsilon_insensitive', 'penalty': 'l2', 'alpha': 0.01},    {'loss': 'epsilon_insensitive', 'penalty': 'l2', 'alpha': 0.001},    {'loss': 'huber', 'penalty': 'l2', 'alpha': 0.1},    {'loss': 'huber', 'penalty': 'l2', 'alpha': 0.01},    {'loss': 'huber', 'penalty': 'l2', 'alpha': 0.001},]
+
+
+best_parameters, best_score= custome_tune_regression_model_hyperparameters(linear_model, training_data[0], training_data[1], dict_hyp)
+print(best_parameters)
+print(best_score)
 # %%
