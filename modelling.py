ee43829fd8f56aa2024c7f83b90f377ef96e0baa
Dennis Yang <dennisyang3.14@gmail.com>
2022-12-21 12:31:24 +0000

    Milestone 4 Task 5

--- modelling.py
+++ modelling.py
@@ -6,7 +6,9 @@ import numpy as np
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from math import sqrt
 from sklearn.model_selection import GridSearchCV
-
+import os
+import json
+import joblib
 
 training_data = load_airbnb("Price_Night")
 # print(training_data)
@@ -58,8 +60,38 @@ dict_hyp = {
 def tune_regression_model_hyperparameters():
     grid_search = GridSearchCV(linear_model, dict_hyp, cv = 5)
     grid_search.fit(training_data[0], training_data[1])
-    print(grid_search.best_params_)
+    # print(grid_search.best_params_)
+    best_parameters = grid_search.best_params_
+    best_rmse = sqrt(-grid_search.best_score_)
 
-tune_regression_model_hyperparameters()
+    return best_parameters, best_rmse
+
+best_parameters, best_rmse = tune_regression_model_hyperparameters()
+tuned_model = SGDRegressor(best_parameters)
+
+def save_model(folder):
+    try:
+        os.mkdir(f"C:\\Users\\denni\\Desktop\\AiCore\\Projects\\modelling-airbnbs-property-listing-dataset-\\models\\regression\{folder}")
+        joblib.dump(tuned_model, f"C:\\Users\\denni\\Desktop\\AiCore\\Projects\\modelling-airbnbs-property-listing-dataset-\\models\\regression\\{folder}\model.joblib")
+
+        with open(f"C:\\Users\\denni\\Desktop\\AiCore\\Projects\\modelling-airbnbs-property-listing-dataset-\\models\\regression\\{folder}\hyperparameters.json", "w") as f:
+            json.dump(best_parameters, f)
+        
+        with open(f"C:\\Users\\denni\\Desktop\\AiCore\\Projects\\modelling-airbnbs-property-listing-dataset-\\models\\regression\\{folder}\metrics.json", "w") as f:
+            json.dump(best_rmse, f)
+
+    except FileExistsError:
+        print("Folder or file already exists, will overwrite with new data")
+        joblib.dump(tuned_model, f"C:\\Users\\denni\\Desktop\\AiCore\\Projects\\modelling-airbnbs-property-listing-dataset-\\models\\regression\\{folder}\model.joblib")
+
+        with open(f"C:\\Users\\denni\\Desktop\\AiCore\\Projects\\modelling-airbnbs-property-listing-dataset-\\models\\regression\\{folder}\hyperparameters.json", "w") as f:
+            json.dump(best_parameters, f)
+        
+        with open(f"C:\\Users\\denni\\Desktop\\AiCore\\Projects\\modelling-airbnbs-property-listing-dataset-\\models\\regression\\{folder}\metrics.json", "w") as f:
+            json.dump(best_rmse, f)
+            
+save_model("linear_regression")
+       
+
 
 # %%
